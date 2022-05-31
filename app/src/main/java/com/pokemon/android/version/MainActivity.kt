4e57b398df68d6160fdfb4bf05e5a3557c9a3625
GabrielBouhnik <gabriel.bouhnik@epita.fr>
2022-05-31 20:58:12 +0200

    add title screen

--- app/src/main/java/com/pokemon/android/version/MainActivity.kt
+++ app/src/main/java/com/pokemon/android/version/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.pokemon.android.version
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
+    var spritesUrl: String = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+    var mediaPlayer: MediaPlayer? = null
+
+    fun displayRandomPokemon(){
+        val random = Random.nextInt(1..225)
+        val imageView : ImageView = findViewById(R.id.randomPokemonSpriteView)
+        Glide.with(this)
+            .load("$spritesUrl$random.png")
+            .into(imageView)
+    }
+
+    fun initGame(){
+        mediaPlayer = MediaPlayer.create(this,R.raw.title_theme)
+        mediaPlayer?.start()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initGame()
+        setContentView(R.layout.title_screen)
+        displayRandomPokemon()
     }
 }
